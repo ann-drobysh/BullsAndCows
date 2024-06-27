1991e6d3771a3188ff11a432e319a66386b13b32
Anna Drobysh <ann.drobysh@yandex.by>
2024-06-27 17:49:29 +0200

    Layout and Rules button logic

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.rules').addEventListener("click", function(){
+    document.querySelector('.rules-modal').showModal();
+});
+document.querySelector('.close-modal-btn').addEventListener("click", function(){
+    document.querySelector('.rules-modal').close();
+});
